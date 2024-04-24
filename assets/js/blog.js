bb4436192b72d2b8dd3b4e9fa2d24f6c3993720b
Nalipas <smoreno2014@gmail.com>
2024-04-24 02:31:25 -0700

    adds backgrounds and additional styles for 'darkModeToggle', sorts blog posts by newest first, and removes padding from various elements

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,14 +1,15 @@
 const blogPosts = JSON.parse(localStorage.getItem('blogPosts')) || [];
 const blogPostsSection = document.getElementById('blogPosts');
 
+blogPosts.sort((a, b) => new Date(b.timestamp) - new Date(a.timestamp));
+
 blogPosts.forEach(post => {
     const postElement = document.createElement('article');
     postElement.id = 'blog-post';
     postElement.innerHTML = `
         <h2>${post.title}</h2>
-        <p><strong>Author:</strong> ${post.username}</p>
         <p>${post.content}</p>
-        <p>${post.timestamp}</p>
+        <p><strong>${post.username}</strong> ${post.timestamp}</p>
     `;
     blogPostsSection.appendChild(postElement);
 });
