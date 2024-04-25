35fae7508eac10673240811032a3d9c64dc72442
Nalipas <smoreno2014@gmail.com>
2024-04-25 11:17:20 -0700

    adds comments to .css and .js files to explain code functionality

--- assets/js/form.js
+++ assets/js/form.js
@@ -4,20 +4,25 @@ const contentInput = document.querySelector('#content');
 const submitPost = document.querySelector('#submit');
 const contentMsg = document.querySelector('#msg');
 
+
+// Displays error or success message.
 function displayMessage(type, message) {
     contentMsg.textContent = message;
     contentMsg.setAttribute('class', type);
-
+    
+    // Sets the error or success message to clear after 5000ms
     setTimeout(function() {
         clearMessage();
     }, 5000);
 }
 
+// Clears error or success message.
 function clearMessage() {
     contentMsg.textContent = '';
     contentMsg.removeAttribute('class');
 }
 
+// Adds event listener to store 'blogPost' data in local storage only after certain conditions are met.
 submitPost.addEventListener('click', function(event) {
     event.preventDefault();
 
@@ -39,11 +44,13 @@ submitPost.addEventListener('click', function(event) {
             timestamp: new Date().toLocaleString()
         };
 
+        // Pulls 'blogPosts' from localStorage, then adds new 'blogPost' to the 'blogPosts' array in localStorage.
         let blogPosts = JSON.parse(localStorage.getItem('blogPosts')) || [];
         blogPosts.push(blogPost);
         localStorage.setItem('blogPosts', JSON.stringify(blogPosts));
         displayMessage('success', 'Blog entry posted!');
 
+        // Adds 3000ms delay to redirect after successful post.
         setTimeout(function() {
             if (contentMsg.getAttribute('class') === 'success') {
                 window.location.href = 'blog.html';
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,10 +1,12 @@
 const darkModeToggle = document.getElementById('darkModeToggle');
 const body = document.body;
 
+// Checks localStorage for 'dark mode' preference. This allows the stlying to persist through each page.
 if (localStorage.getItem('darkMode') === 'enabled') {
     enableDarkMode();
 }
 
+// Adds an event listener to the 'darkModeToggle' button to decide which function to run.
 darkModeToggle.addEventListener('click', function() {
     if (body.classList.contains('dark-mode')) {
         disableDarkMode();
@@ -13,6 +15,7 @@ darkModeToggle.addEventListener('click', function() {
     }
 });
 
+// Adds 'dark-mode' class to body element, enables 'dark mode', and applies styling specific to the button.
 function enableDarkMode() {
     body.classList.add('dark-mode');
     localStorage.setItem('darkMode', 'enabled');
@@ -21,6 +24,7 @@ function enableDarkMode() {
     darkModeToggle.style.border = '2px solid #fff0f5';
 }
 
+// Removes 'dark-mode' class from body element, disables 'dark mode', and applies styling specific to the button.
 function disableDarkMode() {
     body.classList.remove('dark-mode');
     localStorage.setItem('darkMode', null);
